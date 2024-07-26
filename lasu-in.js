626c5f66fc4bc23878658eb1b64ce9b567315e43
Onimisi Adeolu <onimisiadeolu@gmail.com>
2024-07-26 01:00:14 +0100

    Update lasu-in.js
    same reason

--- lasu-in.js
+++ lasu-in.js
@@ -1,23 +1,18 @@
 document.addEventListener("DOMContentLoaded", () => {
     const loginForm = document.getElementById('loginForm');
     const errorMessage = document.getElementById('errorMessage');
-
     loginForm.addEventListener('submit', (event) => {
         event.preventDefault();
-
         const username = document.getElementById('username').value;
         const password = document.getElementById('password').value;
-
         if ((username === 'Reabot6' && password === '220194104') || (username === 'Cyprain' && password === '220194092')) {
             localStorage.setItem('authenticated', 'true');
+            alert('Welcome ' + username)
             window.location.href ='lasu.html';
+        
         } else {
             errorMessage.textContent = 'Invalid username or password.';
             errorMessage.style.display = 'block';
         }
-    });
-
-    if (localStorage.getItem('authenticated') !== 'true') {
-        window.location.href ="lasu-in.html";
-    }
+    });     
 });
